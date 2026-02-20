0ecc01b1b865244d50054ad8e31f0090dfb0294d
Rashi Singh <rub18041977@gmail.com>
2026-02-20 10:33:12 +0530

    java script

---
+++ script.js
@@ -0,0 +1,39 @@
+// console.log("Hello World");
+// document.write("Hello World");
+// var num=10;
+// console.log(num);
+// console.log(typeof(num));
+// num=false;
+// console.log(num);
+// console.log(typeof(num));
+// num="Rashi Singh";
+// console.log(num);
+// console.log(typeof(num));
+// num=null;
+// console.log(num);
+// console.log(typeof(num));
+// num=undefined;
+//function
+var sayHello = function(){
+    alert("Hello world");
+}
+//array
+// var numberArray =[1,2,3];
+// var animals=new Array 
+
+
+
+<sript type="text">
+const sym1= Symbol(4)
+console.log(sym1);
+const sym2= Symbol(4)
+console.log(sym2);
+if(sym1==sym2){
+    console.log("true");
+}
+else{
+    console.log("false")
+}</script>
+
+
+
